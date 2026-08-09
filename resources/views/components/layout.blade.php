205ad1f5a1b96d4a184fcf655e511818207603b0
Zxzidan <dandiazaidane05@gmail.com>
2026-08-09 10:56:49 +0700

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,20 +11,26 @@
 
     <title>@yield('title', 'Sistem Pemesanan Makanan')</title>
 </head>
-<body class="h-full">
-<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> 
 
-<div class="min-h-full">
-  <x-navbar/>
-  <x-header>{{ $title }}</x-header>
- 
-  <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-      {{ $slot }}
+<body class="h-full">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+
+    @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
+
+    <div class="min-h-full">
+        <x-navbar />
+        <x-header>{{ $title }}</x-header>
+
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <!-- Your content -->
+                {{ $slot }}
+            </div>
+        </main>
     </div>
-  </main>
-</div>
 
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home',['nama' => 'Dandi Azaidane', 'title' => 'Home']);
+    return view('home', ['nama' => 'Dandi Azaidane', 'title' => 'Home']);
 });
 
-
-
 Route::get('/menu', function () {
-    return view('menu',['title' => 'Menu']);
+    return view('menu', ['title' => 'Menu']);
 });
 
 Route::get('/order', function () {
-    return view('order',['title' => 'Order']);
+    return view('order', ['title' => 'Order']);
 });
 
 Route::get('/history', function () {
-    return view('history',['title' => 'History']);
+    return view('history', ['title' => 'History']);
 });
 
 Route::get('/reports', function () {
-    return view('reports',['title' => 'Reports']);
+    return view('reports', ['title' => 'Reports']);
 });
