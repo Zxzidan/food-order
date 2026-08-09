d398085456429feb3beb02482ec0a724316ad0c2
Zxzidan <dandiazaidane05@gmail.com>
2026-08-09 10:10:59 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+
+    <p>Welcome, {{ $nama }}!</p>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home',['nama' => 'Dandi Azaidane', 'title' => 'Home']);
+});
+
+
+
+Route::get('/menu', function () {
+    return view('menu',['title' => 'Menu']);
+});
+
+Route::get('/order', function () {
+    return view('order',['title' => 'Order']);
+});
+
+Route::get('/history', function () {
+    return view('history',['title' => 'History']);
+});
+
+Route::get('/reports', function () {
+    return view('reports',['title' => 'Reports']);
+});
