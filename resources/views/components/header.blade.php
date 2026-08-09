1187ae97225048a7d8e93efda02a93320d841c6e
Zxzidan <dandiazaidane05@gmail.com>
2026-08-09 17:41:22 +0700

    Update dropdwon pada header dan menambahkan logo perusahaan pada sidebar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,12 +3,12 @@
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $slot }}</h1>
         <div class="flex items-center">
             <div>
-              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+              <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user" data-dropdown-placement="bottom-end">
                 <span class="sr-only">Open user menu</span>
                 <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
               </button>
             </div>
-            <div class="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44" id="dropdown-user">
+            <div class="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-56" id="dropdown-user">
               <div class="px-4 py-3 border-b border-default-medium" role="none">
                 <p class="text-sm font-medium text-heading" role="none">
                   Neil Sims
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside id="top-bar-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
    <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
-      <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
-         <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="Flowbite Logo" />
-         <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Flowbite</span>
+      <a href="/" class="flex items-center ps-2.5 mb-5">
+         <img src="{{ asset('build/assets/img/LOGO.png') }}" class="h-13 me-3" alt="Garuda Logo" />
+         <span class="self-center text-2xl text-heading font-semibold whitespace-nowrap">Garuda</span>
       </a>
       <ul class="space-y-2 font-medium">
          <li>
