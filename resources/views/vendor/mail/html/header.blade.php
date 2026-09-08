c46c5e6c688954fa18b905868acd2e3af4e146d0
oryzaree <reynaldoryza@gmail.com>
2026-09-08 12:13:26 +0700

    menambahkan logo pada email reset password

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('assets/img/LOGO.png') }}" class="logo" alt="SIPEMMA Logo" style="height: 75px; width: auto; max-width: 100%;">
+</a>
+</td>
+</tr>
